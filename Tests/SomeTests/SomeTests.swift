96144f6490e5552c865a7eed9ec57e797896f4fd
Sven A. Schmidt <sas@finestructure.co>
2024-07-19 13:34:43 +0200

    Trigger crash by forcing the snapshot write

--- Tests/SomeTests/SomeTests.swift
+++ Tests/SomeTests/SomeTests.swift
@@ -7,4 +7,10 @@ class SomeTests: XCTestCase {
     func test_1() async throws {
         assertSnapshot(of: "foo", as: .lines)
     }
+
+    override func invokeTest() {
+        withSnapshotTesting(record: .all, diffTool: .ksdiff) {
+            super.invokeTest()
+        }
+    }
 }
