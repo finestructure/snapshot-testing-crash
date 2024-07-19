064e5f7b8128c1faf72ed8ec80369ed870dc6ace
Sven A. Schmidt <sas@finestructure.co>
2024-07-19 13:26:53 +0200

    Add snapshot-testing dependency

--- Package.swift
+++ Package.swift
@@ -4,8 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "crash-repro",
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.17.2")
+    ],
     targets: [
         .executableTarget(name: "crash-repro"),
-        .testTarget(name: "SomeTests")
+        .testTarget(name: "SomeTests", dependencies: [.product(name: "SnapshotTesting", package: "swift-snapshot-testing")])
     ]
 )
